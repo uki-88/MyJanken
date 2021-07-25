e1ec5dcd55a62d9f54b18c7a67c577d2ec76ff55
uki-88 <uki88.nn.1227@gmail.com>
2021-07-25 22:15:52 +0900

    Initial Commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by 高橋雄輝 on 2021/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 高橋雄輝 on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
